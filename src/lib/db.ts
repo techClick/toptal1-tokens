e2f594e1585b114a02800c545966342ca39076b0
Ikechi Anya <ikechianya1@gmail.com>
2025-12-02 12:40:16 +0100

    added dotenv

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,14 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 export async function initDb(): Promise<void> {
